4d2e582892d3bdf8eb7b9556d76ff0c45a7358ee
Kate <90715013+kujoki@users.noreply.github.com>
2023-04-03 13:26:04 +0300

    Update solution.py
    solution

--- solution.py
+++ solution.py
@@ -2,10 +2,13 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 670223087 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    dt = 56 / (len(x) - 1)
+    v = np.diff(x) / dt
+    a = np.diff(v) / dt
+    
+    mu = np.mean(a)
+    
+    return mu
