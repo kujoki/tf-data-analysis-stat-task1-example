06a541c344f393074d6c17011fd73c115a3c5665
Kate <90715013+kujoki@users.noreply.github.com>
2023-04-03 14:25:40 +0300

    Update solution.py
    median

--- solution.py
+++ solution.py
@@ -5,10 +5,7 @@ import numpy as np
 chat_id = 670223087 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    dt = 56 / (len(x) - 1)
-    v = np.diff(x) / dt
-    a = np.diff(v) / dt
-    
-    mu = np.mean(a)
-    
-    return mu
+    T = 56
+    a = x * 2 / T**2
+    median = np.median(a)
+    return median
